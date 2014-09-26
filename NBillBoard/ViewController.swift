bfeeeb3e3d29513f955c85c8e7f6949c015b02cf
Kedan Li <kedanli@KedantekiMacBook-Pro.local>
2014-09-26 11:45:12 -0500

    Initial Commit

---
+++ NBillBoard/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  NBillBoard
+//
+//  Created by Kedan Li on 14-9-26.
+//  Copyright (c) 2014年 Kedan Li. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
